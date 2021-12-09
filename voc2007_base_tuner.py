725b6cf385b498e4e97bbf3b7ab8d4a73f90eaab
hvarS <srivastavahv@gmail.com>
2021-12-09 08:21:34 +0530

    num trials

--- voc2007_base_tuner.py
+++ voc2007_base_tuner.py
@@ -84,7 +84,7 @@ def objective(trial):
 
 if __name__ == '__main__':
     study = optuna.create_study(direction="maximize",study_name="Optimizer and lr Hypertuning")
-    study.optimize(objective, n_trials=15)
+    study.optimize(objective, n_trials=20)
 
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
